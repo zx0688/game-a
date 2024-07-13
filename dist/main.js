5ae98087765363dedaa8ad3e8d13c0e3000101d0
Evgeny Kutepov <113501488+e-kutepov@users.noreply.github.com>
2024-07-13 13:55:46 +0300

    update

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.httpsOptions = void 0;
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const telegram_service_1 = require("./telegram/telegram.service");
 const swagger_1 = require("@nestjs/swagger");
+const fs = require("fs");
 const cors = require("cors");
+exports.httpsOptions = {
+    key: fs.readFileSync('./secrets/cert.key'),
+    cert: fs.readFileSync('./secrets/cert.crt'),
+};
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions: exports.httpsOptions });
     app.use(cors());
     app.enableCors({
         origin: '*',
--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,13 @@ import * as cors from 'cors';
 
 
 
-// export const httpsOptions = {
-//   key: fs.readFileSync('./secrets/cert.key'),
-//   cert: fs.readFileSync('./secrets/cert.crt'),
-// };
+export const httpsOptions = {
+  key: fs.readFileSync('./secrets/cert.key'),
+  cert: fs.readFileSync('./secrets/cert.crt'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(cors());
   app.enableCors({
     origin: '*',
